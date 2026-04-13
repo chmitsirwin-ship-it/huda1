5292a212c56a31da563a517422d04e4035950bf5
Mohamed Said <personal@msaied.com>
2026-04-13 12:15:05 +0200

    Fix locale handling by using filled() for language input and removing commented code

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -14,23 +14,23 @@ class SetLocale
         $languages = Language::where('is_active', true)->get();
         $activeCodes = $languages->pluck('code')->all();
 
-        if ($request->has('lang') && in_array($request->get('lang'), $activeCodes)) {
-            session(['locale' => $request->get('lang')]);
+        if ($request->has('lang') && in_array($request->filled('lang'), $activeCodes)) {
+            session(['locale' => $request->filled('lang')]);
         }
 
         $locale = session('locale');
 
         $language = match (true) {
             $locale && in_array($locale, $activeCodes) => $languages->firstWhere('code', $locale),
-            in_array($accept = substr($request->header('Accept-Language', ''), 0, 2), $activeCodes) => $languages->firstWhere('code', $accept),
+//            in_array($accept = substr($request->header('Accept-Language', ''), 0, 2), $activeCodes) => $languages->firstWhere('code', $accept),
             default => $languages->firstWhere('is_default', true),
         };
+
 
         if ($language) {
             app()->setLocale($language->code);
             session(['locale' => $language->code]);
         }
-
         return $next($request);
     }
 }
